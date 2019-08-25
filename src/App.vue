e47f50fe35fa23c5ee99f828838318f6c6d63d73
aadm335 <Subhash.Sulanthar@aia.com>
2019-08-25 16:30:27 +1000

    Split global style elements

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <nav>
         <ul>
           <li class="nav-item">
-            <img class="logo" src="./assets/build-a-bot-logo.png" />
+            <img class="logo" src="./assets/build-a-bot-logo.png"/>
           </li>
         </ul>
       </nav>
@@ -33,7 +33,8 @@
     background: linear-gradient(to bottom, #555, #999);
     background-attachment: fixed;
   }
-
+</style>
+<style scoped>
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
   }
@@ -45,21 +46,25 @@
     width: 1024px;
     min-height: 300px;
   }
+
   header {
     background-color: #999;
     width: 1084px;
     margin: 0 auto;
   }
+
   ul {
     padding: 3px;
     display: flex;
   }
+
   .nav-item {
     display: inline-block;
     padding: 5px 10px;
     font-size: 22px;
     border-right: 1px solid #bbb;
   }
+
   .logo {
     vertical-align: middle;
     height: 30px;
