1cb64f81fb683e6d4668e826d19b76168cf1e5b7
AADM335 <subhash.sulanthar@aia.com>
2019-08-26 14:55:31 +1000

    Add links to logo and homepage

--- src/home/HomePage.vue
+++ src/home/HomePage.vue
@@ -5,7 +5,8 @@
            aria-hidden="true"/>
     </div>
     <div class="get-started">
-      <a href="">Get Started</a> building your first robot!
+      <router-link to="/build">Get Started</router-link>
+      building your first robot!
     </div>
   </div>
 </template>
