c92e932b2e0d0859f9daceb09aecac33923ed668
aadm335 <Subhash.Sulanthar@aia.com>
2019-08-27 11:17:21 +1000

    Install vuex and initialize

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
+import store from './store';
 
 import router from './router';
 
@@ -8,4 +9,5 @@ Vue.config.productionTip = false;
 new Vue({
   render: h => h(App),
   router,
+  store,
 }).$mount('#app');
