579d10b5ef52b9ad758f5ab7dbe92bfe7560ffb2
aadm335 <Subhash.Sulanthar@aia.com>
2019-08-27 12:05:19 +1000

    Vuex getters

--- src/cart/ShoppingCart.vue
+++ src/cart/ShoppingCart.vue
@@ -23,15 +23,41 @@
       </tr>
       </tbody>
     </table>
+    <h2 class="sale-items">You saved money on these robots:</h2>
+    <table>
+      <thead>
+      <tr>
+        <th class="robot-title">
+          Robot
+        </th>
+        <th class="cost">
+          Cost
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(robot, index) in cartSaleItems" :key="index">
+        <td class="robot-title">
+          {{robot.head.title}}
+        </td>
+        <td class="cost">
+          {{robot.cost}}
+        </td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
   export default {
     name: 'Cart',
-    computed : {
+    computed: {
       cart() {
         return this.$store.state.cart;
+      },
+      cartSaleItems() {
+        return this.$store.getters.cartSaleItems;
       }
     }
   };
@@ -49,5 +75,11 @@
 
   .cost {
     text-align: right;
+  }
+
+  .sale-items {
+    margin-top: 50px;
+    font-size: 18px;
+    color: red;
   }
 </style>
